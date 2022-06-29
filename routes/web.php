5c0be13bbfa4ccd5a2510250b33c18a9fdbb2ec7
darkopet <darkodepetrovski@gmail.com>
2022-06-29 15:08:56 +0200

    Routes and Files

--- routes/web.php
+++ routes/web.php
@@ -19,11 +19,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-// Route::get('/employees', [new EmployeeController(), 'index']);
-// Route::get('/employees/index', [new EmployeeController(), 'index']);
-// Route::get('/companies', new CompanyController(), 'index');
-// Route::get('/companies/index', new CompanyController(), 'index');
-
 Route::get('/employees', [EmployeeController::class, 'index']);
 Route::get('/employees/index', [EmployeeController::class, 'index']);
 Route::get('/companies', [CompanyController::class, 'index']);
@@ -33,3 +28,21 @@ Route::get('/employees/create', [EmployeeController::class, 'create']);
 Route::post('/employees/create', [EmployeeController::class, 'create']);
 Route::get('/companies/create', [CompanyController::class, 'create']);
 Route::post('/controller/create', [CompanyController::class, 'create']);
+
+Route::get('employees/update', [EmployeeController::class, 'update']);
+Route::post('employees/update', [EmployeeController::class, 'update']);
+Route::get('companies/update', [CompanyController::class, 'update']);
+Route::post('companies/update', [CompanyController::class, 'update']);
+
+Route::post('employees/delete', [EmployeeController::class, 'delete']);
+Route::post('companies/delete', [CompanyController::class, 'delete']);
+
+// Route::get('/employees', [new EmployeeController(), 'index']);
+// Route::get('/employees/index', [new EmployeeController(), 'index']);
+// Route::get('/companies', new CompanyController(), 'index');
+// Route::get('/companies/index', new CompanyController(), 'index');
+
+// Route::get('/employees/create', [new EmployeeController(), 'create']);
+// Route::post('/employees/create', [new EmployeeController(), 'create']);
+// Route::get('/companies/create', [new CompanyController(), 'create']);
+// Route::post('/companies/create', [new CompanyController(), 'create']);
